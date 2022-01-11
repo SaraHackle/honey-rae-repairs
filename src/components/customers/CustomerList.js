1d9aa7d7a015a8885d483c96853521c1e49c32c4
Sara Hackle <Shackle@ivytech.edu>
2022-01-11 10:29:17 -0600

    added a total count of customers

--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-export const  CustomerList = () => {
+export const CustomerList = () => {
   const [customers, assignCustomers] = useState([]);
+  const [totalCustomerMessage, updateMessage] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8088/customers")
@@ -11,12 +12,21 @@ export const  CustomerList = () => {
       });
   }, []);
 
+  useEffect(() => {
+    if (customers.length === 1) {
+      updateMessage("You have 1 customer");
+    } else {
+      updateMessage(`You have ${customers.length} customers`);
+    }
+  }, [customers]);
+
   return (
     <>
-      <h2>Customer List</h2>
-
-      {customers.map((customerObject) => {
-        return <h3 key= {`customer--${customerObject.id}`}>{customerObject.name}</h3>;
+      <div>{totalCustomerMessage}</div>
+      {customers.slice(0, 5).map((customerObject) => {
+        return (
+          <p key={`customer--${customerObject.id}`}>{customerObject.name}</p>
+        );
       })}
     </>
   );
